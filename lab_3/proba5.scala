bf5e54282f8d4d9175ecb9f64e6b066b971e20b1
michal <misiek305@vp.pl>
2018-10-25 15:16:55 +0200

    First new commit xD

--- lab_3/proba5.scala
+++ lab_3/proba5.scala
@@ -1,12 +1,12 @@
 object proba5 {
 
-  def main(args: Array[String])  = {
+  def main(args: Array[String]):Unit  = {
     def eightAndSq(i: Int)= (i*8, i*i)
 
     def humanComp(i: Int, j: Int)= if (i>j) "greater than " else "less than "
     for ( x <- 0 to 10) {
       val (e,s) = eightAndSq(x)
-      println(x.toString + " mul eight " +humanComp(e,s) + x.toString+" sq")
+      println(x.toString + " mul eight siema" +humanComp(e,s) + x.toString+" sq")
     }
   }
 
