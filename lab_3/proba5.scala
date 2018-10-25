1c6238e7ab19d2dc25bf908575bec71672867503
michal <misiek305@vp.pl>
2018-10-25 14:02:10 +0200

    Trying to make GitHub working

---
+++ lab_3/proba5.scala
@@ -0,0 +1,14 @@
+object proba5 {
+
+  def main(args: Array[String])  = {
+    def eightAndSq(i: Int)= (i*8, i*i)
+
+    def humanComp(i: Int, j: Int)= if (i>j) "greater than " else "less than "
+    for ( x <- 0 to 10) {
+      val (e,s) = eightAndSq(x)
+      println(x.toString + " mul eight " +humanComp(e,s) + x.toString+" sq")
+    }
+  }
+
+
+}
