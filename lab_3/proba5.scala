2a9e7084e7c8eca2c393e3b554c77f5c3b499db3
michal <misiek305@vp.pl>
2018-10-25 15:17:24 +0200

    First new commit xD

--- lab_3/proba5.scala
+++ lab_3/proba5.scala
@@ -6,7 +6,7 @@ object proba5 {
     def humanComp(i: Int, j: Int)= if (i>j) "greater than " else "less than "
     for ( x <- 0 to 10) {
       val (e,s) = eightAndSq(x)
-      println(x.toString + " mul eight siema" +humanComp(e,s) + x.toString+" sq")
+      println(x.toString + " mul eight siema a " +humanComp(e,s) + x.toString+" sq")
     }
   }
 
